3d1e57604785b42d3f75ac39b27116cfe849cf4f
Josh Taylor <joshmtaylor2000@gmail.com>
2023-01-25 14:33:44 -0600

    dao module

---
+++ ChessPlayer/src/main/java/com/nashss/se/chessplayerservice/dependency/DaoModule.java
@@ -0,0 +1,29 @@
+package com.nashss.se.chessplayerservice.dependency;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import dagger.Module;
+import dagger.Provides;
+
+import javax.inject.Singleton;
+
+@Module
+public class DaoModule {
+
+    @Singleton
+    @Provides
+    public DynamoDBMapper provideDynamoDBMapper() {
+        return new DynamoDBMapper(
+                AmazonDynamoDBClientBuilder
+                        .standard()
+                        .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
+                        .withRegion(Regions.US_EAST_2)
+                        .build(),
+                DynamoDBMapperConfig.builder()
+                        .withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.UPDATE_SKIP_NULL_ATTRIBUTES)
+                        .build());
+    }
+}
